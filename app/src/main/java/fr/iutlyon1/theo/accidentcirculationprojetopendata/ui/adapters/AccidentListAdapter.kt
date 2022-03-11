35d7d792126c469cb0423eb11710563cbd520d87
theo labrosse <theo.labrosse@etu.univ-lyon1.fr>
2022-03-11 18:50:40 +0100

    replacing the TestAdapter with the AccidentListAdapter

--- app/src/main/java/fr/iutlyon1/theo/accidentcirculationprojetopendata/ui/adapters/AccidentListAdapter.kt
+++ app/src/main/java/fr/iutlyon1/theo/accidentcirculationprojetopendata/ui/adapters/AccidentListAdapter.kt
@@ -8,26 +8,30 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import fr.iutlyon1.theo.accidentcirculationprojetopendata.R
+import fr.iutlyon1.theo.accidentcirculationprojetopendata.modele.Accident
 import java.util.*
 import kotlin.collections.ArrayList
 
-class TestAdapter(
+class AccidentListAdapter(
     val context: Context,
-    val listEquipe : ArrayList<String>
+    val listAccident : ArrayList<Accident>
 ) : BaseAdapter() {
 
 
     override fun getCount(): Int {
-        return listEquipe.size
+        return listAccident.size
     }
 
     override fun getItem(index : Int): Any {
-        return listEquipe[index]
+        return listAccident[index]
     }
 
     override fun getItemId(index: Int): Long {
         return index.toLong()
     }
+
+
+
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
@@ -35,11 +39,10 @@ class TestAdapter(
         val mInflater = LayoutInflater.from(context)
         //(1) : Réutilisation du layout
         if (convertView == null) {
-            layoutItem = mInflater.inflate(R.layout.equipeliste_layout, parent, false)
+            layoutItem = mInflater.inflate(R.layout.accident_card_layout, parent, false)
         } else {
             layoutItem = convertView
         }
-
 
         var viewHolder = ViewHolder (layoutItem.findViewById(R.id.textView))
         if(layoutItem.tag != null) {
@@ -49,12 +52,15 @@ class TestAdapter(
         layoutItem.tag = viewHolder
 
 
-        viewHolder.textView.setText(listEquipe.get(position))
+        viewHolder.textView.setText(listAccident[position].id.toString())
 
 
-        return layoutItem;
+        return layoutItem
     }
+
+
 
 
 }
 class ViewHolder (var textView: TextView)
+
