519910f1511b68449e9e44e75bb68c40b58d3ce1
theo labrosse <theo.labrosse@etu.univ-lyon1.fr>
2022-03-28 14:28:01 +0200

    adding an activity to display the details of the accidents

---
+++ app/src/main/java/fr/iutlyon1/theo/accidentcirculationprojetopendata/accidentDetail.kt
@@ -0,0 +1,11 @@
+package fr.iutlyon1.theo.accidentcirculationprojetopendata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class accidentDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accident_detail)
+    }
+}
